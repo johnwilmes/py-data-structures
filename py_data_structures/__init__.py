f00099706001adf3256a648ab4d28dfbf1756456
John Wilmes <jw@johnwilmes.name>
2017-03-15 12:31:41 -0400

    Reorganize code into package
    Organize the code into a package according to usual Pytyhon best practices.
    This way code can be consistently imported, e.g. in tests.

---
+++ py_data_structures/__init__.py
@@ -0,0 +1,6 @@
+"""This module provides basic data structures and algorithms implemented in
+Python, for fun."""
+
+from py_data_structures.trie import Trie, StringTrie
+from py_data_structures.llist import LList
+from py_data_structures.slist import SList
